ae1715644975b7bcacf6b72d190445f8341fe70a
Peter Amezquita <38058907+amezquitap@users.noreply.github.com>
2018-12-09 23:41:21 -0500

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,165 @@
+// friends data
+var friends = [
+	{
+		name: 'Jose',
+		photo: 'http://slavablazerphotography.com/wp-content/uploads/2017/03/IMG_6127_copy.jpg',
+		scores: [
+			'1',
+			'2',
+			'5',
+			'1',
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Mya',
+		photo: 'https://static1.squarespace.com/static/54ab66dee4b0da465dd06039/54e034fbe4b05fac69eec459/554434d4e4b0a65de4ee8fe5/1430533339238/Mya50563-Edit-Edit-3.jpg',
+		scores: [
+			'2',
+			'3',
+			'1',
+			'1',
+			'4',
+			'3',
+			'1',
+			'2',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Sean',
+		photo: 'https://static1.squarespace.com/static/57809f1fe6f2e12e1da3e283/57831f13ff7c501754f8277e/5783ed3c46c3c44b31cc926a/1525375257905/Headshots_Men38.jpg?format=500w',
+		scores: [
+			'3',
+			'3',
+			'2',
+			'4',
+			'1',
+			'1',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Sue',
+		photo: 'https://static1.squarespace.com/static/53b599ebe4b08a2784696956/53c987b9e4b02a3a05287045/59b0c49b914e6b2482bb0bd1/1504758143634/professional-headshots-nyc-029.jpg',
+		scores: [
+			'2',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'2',
+			'3',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Joe',
+		photo: 'https://static1.squarespace.com/static/593d7dea3e00be288f73097b/5a1de564f9619afa6acb8e6e/5a1de5e0085229dccc30e9e5/1515831410618/1_20171107_DSC02512_full+res_J-Squared-Headshots-Newport_Joseph-Barber-Photography.jpg?format=1000w',
+		scores: [
+			'1',
+			'3',
+			'5',
+			'4',
+			'5',
+			'5',
+			'4',
+			'3',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Maria',
+		photo: 'https://format-com-cld-res.cloudinary.com/image/private/s--THXwaGs8--/c_limit,g_center,h_65535,w_700/a_auto,fl_keep_iptc.progressive,q_95/v1/6fec92885f5cbfb6e383542eff845a6b/IMG_9076.jpg',
+		scores: [
+			'1',
+			'1',
+			'2',
+			'1',
+			'3',
+			'5',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Alexandra',
+		photo: 'https://www.smartheadshots.com/blog/photos/actor-headshots-1283.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'John',
+		photo: 'https://i.pinimg.com/originals/29/80/94/298094c2d1dbf6267d5e61d741b902c2.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'2',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sophia',
+		photo: 'https://www.smartheadshots.com/actor-headshots/headshots/headshots-5225.jpg',
+		scores: [
+			'4',
+			'4',
+			'2',
+			'1',
+			'1',
+			'3',
+			'5',
+			'3',
+			'5',
+			'2'
+		]
+	},
+	{
+		name: 'Brandon',
+		photo: 'https://static1.squarespace.com/static/590814dc1e5b6c7f60b5e133/590825ccbe6594bddd3d13be/592cef0bd2b857159c9d2d48/1496288697907/matthew_011.jpg',
+		scores: [
+			'2',
+			'1',
+			'4',
+			'2',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'1'
+		]
+	}
+];
+
+module.exports = friends;
